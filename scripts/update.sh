6bb99d1a209cfb60de8570c4482992952d8b4647
Jürno Ader <jyrno42@gmail.com>
2015-09-11 13:34:57 +0300

    update docs to 0.2.2

--- scripts/update.sh
+++ scripts/update.sh
@@ -16,16 +16,18 @@ echo "Found $version"
 
 # Make temporary directory
 rm -rf .tmp
-mkdir .tmp
+mkdir -p .tmp
 
 # Copy ~/dist/examples dir from installed package
-cp ./node_modules/tg-modal/dist/examples .tmp
+cp ./node_modules/tg-modal/dist/examples/* .tmp/
 
 # move .tmp to ../$version
-mv .tmp ../$version
+mkdir -p ../$version
+mv .tmp/* ../$version
 
 # Remove previous link
-rm ../latest
+rm -f ../latest
 
 # Create a symlink from ../$version to ../latest
-sudo ln -s ../$version/ ../latest
+cd ..
+ln -s $version latest
