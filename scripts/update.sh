a0564e8065c5f8c038216b908daa5c069a0064a1
Jürno Ader <jyrno42@gmail.com>
2015-09-11 13:10:52 +0300

    prep for new docs style

---
+++ scripts/update.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# ensure paths exists
+mkdir -p ./node_modules
+mkdir -p ./node_modules/tg-modal
+
+# Remove ./node_modules/tg-modal
+rm -rf ./node_modules/tg-modal
+
+# Install latest available tg-modal
+npm install tg-modal@latest > /dev/null 2>&1
+
+# Get version of installed tg-modal
+version=`npm list tg-modal | grep "tg-modal@" | sed -r 's/.+tg\-modal\@//g'`
+echo "Found $version"
+
+# Make temporary directory
+rm -rf .tmp
+mkdir .tmp
+
+# Copy ~/dist/examples dir from installed package
+cp ./node_modules/tg-modal/dist/examples .tmp
+
+# move .tmp to ../$version
+mv .tmp ../$version
+
+# Remove previous link
+rm ../latest
+
+# Create a symlink from ../$version to ../latest
+sudo ln -s ../$version/ ../latest
